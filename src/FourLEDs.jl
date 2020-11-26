a3af43fe26d6d2bf334a7fde8d534ff1d5a85223
Yakir Gagnon <12.yakir@gmail.com>
2020-11-26 16:28:07 +0100

    done

--- src/FourLEDs.jl
+++ src/FourLEDs.jl
@@ -5,19 +5,7 @@ using GtkReactive, Gtk.ShortNames, LibSerialPort, COBS
 export main
 
 baudrate = 115200
-
-function get_port()
-    for port in LibSerialPort.get_port_list()
-        try 
-            sp = LibSerialPort.open(port, baudrate)
-            sleep(3)
-            encode(sp, [0x00,0x00])
-            return sp
-        catch ex
-        end
-    end
-    return nothing
-end
+portfile = joinpath(DEPOT_PATH[1], "fourledsport")
 
 function connect_slider(sp, id)
     sl = slider(0x00:0xff, value = 0x00)
@@ -27,59 +15,64 @@ function connect_slider(sp, id)
     return sl
 end
 
+function get_current_port_list(vb, msg, ok, win)
+    hb = Box(:h)
+    push!(vb, hb)
+    msg = label(msg)
+    ok = button(ok)
+    push!(hb, msg)
+    push!(hb, ok)
+    ports = map(ok) do _
+        LibSerialPort.get_port_list()
+    end
+    Gtk.showall(win)
+    c = Condition()
+    h = map(ok) do _
+        notify(c)
+    end
+    wait(c)
+    destroy(hb)
+    return value(ports)
+end
+
+function get_port(vb, win)
+    without = get_current_port_list(vb, "Is the LED strip disconnected?", "Yes", win)
+    with = get_current_port_list(vb, "Now, connect the LED strip", "OK", win)
+    port =  only(setdiff(with, without))
+    open(portfile, "w") do o
+        println(o, port)
+    end
+    return port
+end
+
+function get_sp(vb, win)
+    if isfile(portfile) 
+        port = readline(portfile) 
+        try 
+            sp = LibSerialPort.open(port, baudrate)
+            return sp
+        catch ex
+            rm(portfile)
+        end
+    end
+    port = get_port(vb, win)
+    sleep(1)
+    return LibSerialPort.open(port, baudrate)
+end
+
+
 function main()
     win = Window("FourLEDs") 
     vb = Box(:v)
     push!(win, vb)
-    hb = Box(:h)
-    push!(vb, hb)
-    msg = label("Is the LED strip disconnected?")
-    ok = button("Yes")
-    push!(hb, msg)
-    push!(hb, ok)
 
-    without = map(ok) do _
-        LibSerialPort.get_port_list()
+    sp = get_sp(vb, win)
+
+    for i in 0x00:0x03
+        push!(vb, connect_slider(sp, i))
     end
+
     Gtk.showall(win)
-    c = Condition()
-    h = map(ok) do _
-        notify(c)
-    end
-    wait(c)
-
-    destroy(hb)
-
-    hb = Box(:h)
-    push!(vb, hb)
-    msg = label("Now, connect the LED strip")
-    ok = button("OK")
-    push!(hb, msg)
-    push!(hb, ok)
-    with = map(ok) do _
-        LibSerialPort.get_port_list()
-    end
-    Gtk.showall(win)
-    c = Condition()
-    h = map(ok) do _
-        notify(c)
-    end
-    wait(c)
-
-
-    h = map(with, without) do w, wo
-        @show with, without
-        for port in setdiff(w, wo)
-            push!(msg, "")
-            sp = LibSerialPort.open(port, baudrate)
-
-            for i in 0x00:0x03
-                push!(vb, connect_slider(sp, i))
-            end
-
-            Gtk.showall(win)
-        end
-    end
 
     if !isinteractive()
         Gtk.waitforsignal(win, :destroy)
